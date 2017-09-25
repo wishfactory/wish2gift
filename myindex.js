c7d3041e8d205b693f78175c8742f139d944527d
Abhishek Gupta <guptabhi.mait@gmail.com>
2017-09-26 03:03:26 +0530

    Form_Wish2Gift
    It is the final form with little bit of javascript functionality.

---
+++ myindex.js
@@ -0,0 +1,32 @@
+function show1(){
+  document.getElementById('div0').style.display = 'none';
+  document.getElementById('div1').style.display = 'block';
+ }
+ function show2(){
+   document.getElementById('div0').style.display = 'none';
+   document.getElementById('div2').style.display = 'block';
+  }
+  function show3(){
+    document.getElementById('div0').style.display = 'none';
+    document.getElementById('div3').style.display = 'block';
+   }
+   function show4(){
+     document.getElementById('div0').style.display = 'none';
+     document.getElementById('div4').style.display = 'block';
+    }
+    function show5(){
+      document.getElementById('div0').style.display = 'none';
+      document.getElementById('div5').style.display = 'block';
+     }
+     function show6(){
+       document.getElementById('div0').style.display = 'none';
+       document.getElementById('div6').style.display = 'block';
+      }
+      function show7(){
+        document.getElementById('div0').style.display = 'none';
+        document.getElementById('div7').style.display = 'block';
+       }
+       function show8(){
+         document.getElementById('div0').style.display = 'none';
+         document.getElementById('div8').style.display = 'block';
+       }
